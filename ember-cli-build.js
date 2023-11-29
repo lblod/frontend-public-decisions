a9ffd8c0ed7d5213df992ed7ff55d0c391534af3
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2023-11-29 11:56:37 +0100

    Update to ESA v6
    This includes a fix for Ember v5

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,10 +4,6 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    // Add options here
-    'ember-simple-auth': {
-      useSessionSetupMethod: true,
-    },
     '@appuniversum/ember-appuniversum': {
       disableWormholeElement: true,
       dutchDatePickerLocalization: true,
